3f79fc759a75a1acce9af8e86868cf2f8495be5a
Martin Englund <martin@englund.nu>
2015-03-29 13:10:30 -0700

    use consul to get location of redis

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'sidekiq'
-
+gem 'diplomat'
--- lib/skalera/sidekiq.rb
+++ lib/skalera/sidekiq.rb
@@ -1,4 +1,19 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://172.16.57.179:6379/0' }
+require 'sidekiq'
+require 'diplomat'
+
+consul = ENV['CONSUL'] || 'consul'
+
+begin
+  Diplomat.configuration.url = ("http://#{consul}:8500")
+  redis = Diplomat::Service.get('redis')
+rescue Faraday::ConnectionFailed
+  STDERR.puts("ERROR: could not lookup host #{consul}")
+  exit(1)
+rescue Faraday::ResourceNotFound
+  STDERR.puts("ERROR: could not lookup consul service redis")
+  exit(1)
 end
 
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://#{redis.Address}:#{redis.ServicePort}/0"}
+end
